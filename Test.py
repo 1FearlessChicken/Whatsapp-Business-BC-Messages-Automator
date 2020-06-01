a152d1a70e48d56c5e3459274f12aaebac055613
1FearlessChicken <55254530+1FearlessChicken@users.noreply.github.com>
2020-06-02 00:04:49 +0100

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client 
+ 
+account_sid = 'ACf717a2a13c03821aa5976151196139c1' 
+auth_token = '9e63223620e8b5540ec7969cc156b1ef' 
+client = Client(account_sid, auth_token) 
+ 
+message = client.messages.create( 
+                              from_='whatsapp:+14155238886',
+                              body='Test Message For Whatsapp BC database',      
+                              to='whatsapp:+2349091425496'
+                          ) 
+ 
+print(message.sid)
